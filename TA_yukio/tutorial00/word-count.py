029a7c024e631d4d9363bdd7cc9be87437b3a95b
Yukio Matsumura <Yukio@Yukio-no-MacBook-Air.local>
2017-04-21 20:19:28 +0900

    test

--- TA_yukio/tutorial00/word-count.py
+++ TA_yukio/tutorial00/word-count.py
@@ -14,6 +14,5 @@ def word_count(fin_path):
         for word, freq in sorted(word_count.items(), key = lambda x: x[0]):
             print("{} {}".format(word, freq))
 
-
 if __name__ == "__main__":
     word_count(sys.argv[1])
