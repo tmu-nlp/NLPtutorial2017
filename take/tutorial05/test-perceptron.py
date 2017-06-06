d95b2224c646d7d2694e4f1fd1d134e205444acb
take <take>
2017-06-06 14:50:14 +0900

    remove commented line.

--- take/tutorial05/test-perceptron.py
+++ take/tutorial05/test-perceptron.py
@@ -2,8 +2,6 @@ from train_perceptron import create_features, predict_one
 from collections import defaultdict
 import sys
 
-# modelfile = 'iter_1.model'
-# modelfile = 'iter_40.model'
 modelfile = sys.argv[1]
 inputfile = '../../data/titles-en-test.word'
 ansfile = modelfile + '_ans'
@@ -15,13 +13,11 @@ def pred_all(model_file, input_file):
         w = defaultdict(lambda: 0)
         for model_line in modelf:
             name, weight = model_line.split('\t')
-            # print("{}\t{}".format(name,weight))
             w[name] = int(weight)
 
     with open(input_file) as inputf, open(ansfile, 'w') as ansout:
         for in_line in inputf:
             l = in_line
-            # print(l)
             phi = create_features(l.lower())
             y_pred = int(predict_one(w,phi))
             print('{}\t{}'.format(y_pred, l), end='', file=ansout)
@@ -31,4 +27,3 @@ if __name__ == '__main__':
 
     import subprocess
     subprocess.call('../../script/grade-prediction.py ../../data/titles-en-test.labeled '+ ansfile, shell=True)
-    # ../../script/grade-prediction.py ../../data/titles-en-test.labeled 20.ans
