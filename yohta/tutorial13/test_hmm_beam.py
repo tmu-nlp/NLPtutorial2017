1efa8ec49452b2f0d10dbdb9376db0b44a839676
長内洋太 <>
2017-07-25 15:27:20 +0900

    12 is heavy

--- yohta/tutorial13/test_hmm_beam.py
+++ yohta/tutorial13/test_hmm_beam.py
@@ -7,7 +7,7 @@ lambda1 = 0.1
 B = 20
 
 with open('../tutorial04/model-hmm.txt') as model_file:
-    trans = defaultdict(lambda: 1)
+    trans = defaultdict(lambda :1)
     emit = defaultdict(float)
     possible_tags = dict()
     for line in model_file:
@@ -40,12 +40,11 @@ with open('../../data/wiki-en-test.norm') as text:
                             best_edge[str(i + 1) + ' ' + nex] = str(i) + ' ' + prev
                             my_best[nex] = score
             tags.append([])
+
             for key, value in sorted(my_best.items(), key = lambda x:x[1]):
                 tags[i + 1].append(key)
                 if len(tags[i + 1]) == B:
                   break
-
-
 
         for prev in tags[i]:
             if (str(len(words)) + ' ' + prev) in best_score:
@@ -53,9 +52,9 @@ with open('../../data/wiki-en-test.norm') as text:
                 if (str(len(words) + 1) + ' ' + '</s>') not in best_score or best_score[str(len(words) + 1) + ' ' + '</s>'] > score:
                     best_score[str(len(words) + 1) + ' ' + '</s>'] = score
                     best_edge[str(len(words) + 1) + ' ' + '</s>'] = str(len(words)) + ' ' + prev
-
         tags = []
         next_edge = best_edge[str(l + 1)  + ' ' + '</s>']
+
         while next_edge != '0 <s>':
             position, tag = next_edge.split()
             tags.append(tag)
